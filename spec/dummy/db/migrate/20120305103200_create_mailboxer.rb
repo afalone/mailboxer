af821becdf06312ffaf6fcbadcf23846e24fd60c
anton_prokhorov <anton_prokhorov@etb-lab.com>
2014-08-23 15:47:34 +0400

    remove foreigner from dependency

--- spec/dummy/db/migrate/20120305103200_create_mailboxer.rb
+++ spec/dummy/db/migrate/20120305103200_create_mailboxer.rb
@@ -10,7 +10,7 @@ class CreateMailboxer < ActiveRecord::Migration
   	#Receipts
     create_table :mailboxer_receipts do |t|
       t.references :receiver, :polymorphic => true
-      t.column :notification_id, :integer, :null => false
+      t.column :notification_id, :integer, :null => false, index: true
       t.column :is_read, :boolean, :default => false
       t.column :trashed, :boolean, :default => false
       t.column :deleted, :boolean, :default => false
@@ -26,7 +26,7 @@ class CreateMailboxer < ActiveRecord::Migration
       t.references :sender, :polymorphic => true
       t.references :notified_object, :polymorphic => true
       t.string :notification_code, :default => nil
-      t.column :conversation_id, :integer
+      t.column :conversation_id, :integer, index: true
       t.column :draft, :boolean, :default => false
       t.column :attachment, :string
       t.column :updated_at, :datetime, :null => false
@@ -34,30 +34,10 @@ class CreateMailboxer < ActiveRecord::Migration
       t.boolean :global, :default => false
       t.datetime :expires
     end    
-    
-    
-  #Indexes
-  	#Conversations
-  	#Receipts
-  	add_index "mailboxer_receipts","notification_id"
-
-  	#Messages  
-  	add_index "mailboxer_notifications","conversation_id"
-  
-  #Foreign keys    
-  	#Conversations
-  	#Receipts
-  	add_foreign_key "mailboxer_receipts", "mailboxer_notifications", :name => "receipts_on_notification_id", :column => "notification_id"
-  	#Messages  
-  	add_foreign_key "mailboxer_notifications", "mailboxer_conversations", :name => "notifications_on_conversation_id", :column => "conversation_id"
   end
   
   def self.down
   #Tables  	
-  	remove_foreign_key "mailboxer_receipts", :name => "receipts_on_notification_id"
-  	remove_foreign_key "mailboxer_notifications", :name => "notifications_on_conversation_id"
-  	
-  #Indexes
     drop_table :mailboxer_receipts
     drop_table :mailboxer_conversations
     drop_table :mailboxer_notifications
